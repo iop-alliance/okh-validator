7183b7bde6ee7ce5b2dfff2456c9f05b14af727a
Max Mahmoud Wardeh <max@wardeh.org>
2022-07-14 11:04:31 +0100

    Rename manifest author validation private functions

--- lib/okh_validator_web/live/validation_helpers/manifest_author.ex
+++ lib/okh_validator_web/live/validation_helpers/manifest_author.ex
@@ -7,17 +7,17 @@ defmodule OkhValidatorWeb.ValidationHelpers.ManifestAuthor do
     show_manifest_author_status(validatons.field_validations["manifest-author"])
   end
 
-  defp show_manifest_author_status(%{status: "ok"} = manifest_author), do: check_manifest_author_validations(manifest_author.validations)
+  defp show_manifest_author_status(%{status: "ok"} = manifest_author), do: show_manifest_author_validations(manifest_author.validations)
   defp show_manifest_author_status(%{status: status}), do: status
 
-  defp check_manifest_author_validations(%{status: "ok"} = manifest_author_validations) do
+  defp show_manifest_author_validations(%{status: "ok"} = manifest_author_validations) do
     raw(
       "<ul><li>Name: #{TextField.show_validation_result(manifest_author_validations.validations.name)}</li>" <>
       "<li>Email: #{TextField.show_validation_result(manifest_author_validations.validations.email)}</li>" <>
       "<li>Affiliation: #{TextField.show_validation_result(manifest_author_validations.validations.affiliation)}</li></ul>"
     )
   end
-  defp check_manifest_author_validations(%{status: "error"} = manifest_author_validations) do
+  defp show_manifest_author_validations(%{status: "error"} = manifest_author_validations) do
     raw("<span class=\"text-danger\">Error: #{manifest_author_validations.message}</span>")
   end
 end
