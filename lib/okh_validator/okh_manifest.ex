d70fda600a0ac19d32595b7f13dde099c17869ce
Max Mahmoud Wardeh <max@wardeh.org>
2022-07-19 12:19:16 +0100

    Refactor manifest validator pipeline

--- lib/okh_validator/okh_manifest.ex
+++ lib/okh_validator/okh_manifest.ex
@@ -15,66 +15,66 @@ defmodule OkhValidator.OkhManifest do
     UrlValidator
   }
 
+  @date_fields ["date-created", "date-updated"]
+  @text_fields [
+    "title",
+    "description",
+    "intended-use",
+    "version",
+    "health-safety-notice",
+    "development-stage",
+    "derivative-of", # TODO: use project validator (https://standards.internetofproduction.org/pub/okh#derivative-of)
+    "variant-of", # TODO: use project validator (https://standards.internetofproduction.org/pub/okh#derivative-of)
+    "sub"] # TODO: use project validator (https://standards.internetofproduction.org/pub/okh#derivative-of)
+  @array_fields [
+    "keywords",
+    "standards-used"] # TODO: create standards validator
+  @url_fields [
+    "project-link",
+    "image",
+    "documentation-home",
+    "archive-download",
+    "bom",
+    "tool-list"]
+  @boolean_fields [
+    "made",
+    "made-independently"]
+  @link_list_fields [ # TODO: implement link list validator
+    "design-files",
+    "schematics",
+    "making-instructions",
+    "manufacturing-files",
+    "risk-assessment",
+    "tool-settings"  ,
+    "quality-instructions",
+    "operating-instructions",
+    "maintenance-instructions",
+    "disposal-instructions",
+    "software"]
+  @language_fields [ # TODO: implement language validator
+    "manifest_language",
+    "documentation_language",
+    "documentation_language"]
+  @translation_fields [ # TODO: implement language validator
+    "manifest-is-translation",
+    "documentation-is-translation"]
+
   def validate_manifest(manifest_fields) do
     field_validations =
       %{}
-      |> DateValidator.validate_date(manifest_fields, [
-          "date-created", "date-updated"
-        ])
+      |> DateValidator.validate_date(manifest_fields, @date_fields)
       |> ManifestAuthorValidator.validate_manifest_author(manifest_fields)
-      |> TextValidator.validate_text(manifest_fields, [
-          "title",
-          "description",
-          "intended-use",
-          "version",
-          "health-safety-notice",
-          "development-stage",
-          "derivative-of", # TODO: use project validator (https://standards.internetofproduction.org/pub/okh#derivative-of)
-          "variant-of", # TODO: use project validator (https://standards.internetofproduction.org/pub/okh#derivative-of)
-          "sub" # TODO: use project validator (https://standards.internetofproduction.org/pub/okh#derivative-of)
-        ])
+      |> TextValidator.validate_text(manifest_fields, @text_fields)
       |> LicenseValidator.validate_license(manifest_fields)
-      |> ArrayValidator.validate_array_field(manifest_fields, [
-          "keywords",
-          "standards-used" # TODO: create standards validator
-        ])
-      |> UrlValidator.validate_url(manifest_fields, [
-          "project-link",
-          "image",
-          "documentation-home",
-          "archive-download",
-          "bom",
-          "tool-list"
-        ])
+      |> ArrayValidator.validate_array_field(manifest_fields, @array_fields)
+      |> UrlValidator.validate_url(manifest_fields, @url_fields)
       |> ContactValidator.validate_contact(manifest_fields)
       |> ContributorsValidator.validate_contributors(manifest_fields)
-      |> BooleanValidator.validate_boolean(manifest_fields, [
-          "made",
-          "made-independently"
-        ])
+      |> BooleanValidator.validate_boolean(manifest_fields, @boolean_fields)
       |> PersonValidator.validate_person_field(manifest_fields, "licensor") # TODO: use license validator (https://standards.internetofproduction.org/pub/okh#license)
-      |> LinkListValidator.validate_link_list(manifest_fields, [ # TODO: implement link list validator
-          "design-files",
-          "schematics",
-          "making-instructions",
-          "manufacturing-files",
-          "risk-assessment",
-          "tool-settings"  ,
-          "quality-instructions",
-          "operating-instructions",
-          "maintenance-instructions",
-          "disposal-instructions",
-          "software"
-        ])
-      |> LanguageValidator.validate_language(manifest_fields, [ # TODO: implement language validator
-          "manifest_language",
-          "documentation_language",
-          "documentation_language"
-        ])
-      |> TranslationValidator.validate_translation_field(manifest_fields, [ # TODO: implement language validator
-          "manifest-is-translation",
-          "documentation-is-translation"
-        ])
+      |> LinkListValidator.validate_link_list(manifest_fields, @link_list_fields)
+      |> LanguageValidator.validate_language(manifest_fields, @language_fields)
+      |> TranslationValidator.validate_translation_field(manifest_fields, @translation_fields)
 
     manifest_validation = validate_required_fields(field_validations)
 
